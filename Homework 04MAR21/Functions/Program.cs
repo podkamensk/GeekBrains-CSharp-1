9e33d7dc30e1c9cb7eacd8d9fd19b352e7c00161
rpatarushin <roman.patarushin@yahoo.com>
2021-03-10 14:47:07 +0000

    Homwork 6 - not finished yet :( )

---
+++ Homework 04MAR21/Functions/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public delegate double Fun(double x, double j);
+
+    class Program
+    {   
+
+        public static void Table(Fun F, double a, double x, double b)
+        //Выыод значений функции в диапазоне
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+
+        public static double MySin(double a, double x)
+        {
+            return a*Math.Sin(x);
+        }
+        public static double MySqr(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            /* ПАТАРУШИН
+            1. Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). 
+            Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+            */
+
+            // Демонстрация фунции a*sin(x)
+            Console.WriteLine("Таблица функции a*sin(x):");
+            Table(new Fun(MySin), 4, -2, 2);
+
+            // Демонстрация фунции a*sqr(x)
+            Console.WriteLine("Таблица функции a*sqr(x):");
+            Table(new Fun(MySqr), 4, -2, 2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework 04MAR21/MinFunc/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+public delegate double Funс(double x);
+
+
+
+namespace MinFunc
+{
+    class Program
+    {
+        public static double Func_Quadratic(double x, double a, double b, double c)
+        {
+            return a*x*x + b*x + c;
+        }
+
+        public static double Func_Sin(double x, double a, double b, double phase)
+        {
+            return a*Math.Sin(b*x + phase);
+        }
+
+        public static double Func_Linear(double x, double k, double b)
+        {
+            return k*x + b;
+        }
+
+        public static double Func_Cubic(double x, double a, double b, double c, double d)
+        {
+            return a*x*x*x + b*x*x + c*x + d;
+        }
+
+
+        public static void SaveFunc(string fileName, double a, double b, double h)
+        //Saves resuts of a function in range of X to the file
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        public static double Load(string fileName)
+        //Reads the results from file, then returns min value
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            SaveFunc("data.bin", -100, 100, 0.5);
+            Console.WriteLine(Load("data.bin"));
+            Console.ReadKey();
+
+        }
+    }
+}
